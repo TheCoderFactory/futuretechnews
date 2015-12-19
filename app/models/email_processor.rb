3f2d4e3adef11c9a48943fb98359670fa12f1f7b
Pete <pete@coderfactory.com>
2015-12-20 02:30:51 +1100

    fix thumb

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,11 +1,12 @@
 class EmailProcessor
+	# require 'email_thumbnailer'
   def initialize(email)
     @email = email
   end
 
   def process
     # all of your code to deal with the email goes here
-    page = Thumbnailer.generate(@email.body)
+    page = LinkThumbnailer.generate(@email.body)
     NewsItem.create!(title: page.title, favicon: page.favicon, description: page.description, main_image: page.images.first.src.to_s)
   end
 end
