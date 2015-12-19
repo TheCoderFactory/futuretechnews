82487ab34e85642512eeb7a759caf03c41d17e05
Pete <pete@coderfactory.com>
2015-12-20 02:33:57 +1100

    fix thumb

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -7,6 +7,6 @@ class EmailProcessor
   def process
     # all of your code to deal with the email goes here
     page = LinkThumbnailer.generate(@email.body)
-    NewsItem.create!(title: page.title, favicon: page.favicon, description: page.description, main_image: page.images.first.src.to_s)
+    NewsItem.create!(url: page.url, title: page.title, favicon: page.favicon, description: page.description, main_image: page.images.first.src.to_s)
   end
 end
