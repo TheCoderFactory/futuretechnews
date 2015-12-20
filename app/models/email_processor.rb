2063c92bdeb4ac89d9bf1f7c3e764bb4ff2ab72a
pedorgrande <pete@thecoderfactory.com>
2015-12-20 19:01:00 +1100

    h

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -7,7 +7,7 @@ class EmailProcessor
   def process
     # all of your code to deal with the email goes here
     page = LinkThumbnailer.generate(@email.body)
-    user_identity = Identity.find_by(email: @email.from)
+    user_identity = Identity.find_by(email: @email.from.email)
     # user = User.find_by(email: @email.from)
     if user_identity
     	NewsItem.create!(url: @email.body, title: page.title, favicon: page.favicon, description: page.description, main_image: page.images.first.src.to_s, tag_list: @email.subject, user_id: user_identity.id)
